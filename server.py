03f1f99820fb54b4998655218dbe4d9eeed5a6fa
sadistic <saad778964321@gmail.com>
2026-07-14 23:15:49 +0530

    added monkey_patch

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from database import save_category,get_seller_analytics,get_resturantItem_price,add_subcategory,add_resturant_items,check_existing_owner,set_verified,fetch_address,add_resturants,list_resturant_items,list_resturants,add_customer_items,update_resturant_item,remove_itemss,store_orders,get_orders,store_seller_orders,get_seller_ordes,check_existing_user,create_new_user,update_order_status_seller,update_order_status_user,resturant_stats,return_res_analytics,check_existing_owner,save_address, verify_order
 from flask import Flask,request,render_template,redirect,url_for,jsonify,g
 from flask_socketio import SocketIO, emit,join_room
